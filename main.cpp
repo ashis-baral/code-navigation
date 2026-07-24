04d28a91d514e7f7e02ac28ece7fdfa7bd45afd2
Ashis Kumar Baral <learnreality0@gmail.com>
2026-07-24 19:39:58 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
-#include <iostream>
-
-int add(int a, int b) {
-    return a + b;
-}
-
-int main() {
-    int result = add(2, 3);
-    std::cout << result << std::endl;
-    return 0;
-}
+class Calculator {
+public:
+    int add(int a, int b) {
+        return a + b;
+    }
+};
